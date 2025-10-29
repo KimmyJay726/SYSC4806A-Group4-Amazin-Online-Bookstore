c164ee60865a625e079cfec7e5325c1bf34d12eb
Indecisive613 <fvcheng613@gmail.com>
2025-10-29 10:49:49 -0400

    feat: Add functional login and sign up pages

--- src/test/java/org/example/ClientTest.java
+++ src/test/java/org/example/ClientTest.java
@@ -87,7 +87,7 @@ public class ClientTest {
         // Add the book to the cart
         client.addToShoppingCart(book1);
         Assertions.assertEquals(1, client.getShoppingCart().size());
-        Assertions.assertTrue(client.getShoppingCart().contains(book1));
+        Assertions.assertTrue(client.getShoppingCart().contains(book1.getId()));
     }
 
     @Test
@@ -130,6 +130,6 @@ public class ClientTest {
         // Add the book to purchase list
         client.addToPurchasedBooks(book1);
         Assertions.assertEquals(1, client.getPurchasedBooks().size());
-        Assertions.assertTrue(client.getPurchasedBooks().contains(book1));
+        Assertions.assertTrue(client.getPurchasedBooks().contains(book1.getId()));
     }
 }
